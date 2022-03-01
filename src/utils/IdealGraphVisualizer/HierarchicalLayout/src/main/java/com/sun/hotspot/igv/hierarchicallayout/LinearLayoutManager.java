7b44e410ab6f764e60d955541eea7151a3a531a4
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2022-03-01 10:23:50 +0100

    Reduce LinearLayoutManager to its core

--- src/utils/IdealGraphVisualizer/HierarchicalLayout/src/main/java/com/sun/hotspot/igv/hierarchicallayout/LinearLayoutManager.java
+++ src/utils/IdealGraphVisualizer/HierarchicalLayout/src/main/java/com/sun/hotspot/igv/hierarchicallayout/LinearLayoutManager.java
@@ -27,45 +27,16 @@ import com.sun.hotspot.igv.layout.LayoutGraph;
 import com.sun.hotspot.igv.layout.LayoutManager;
 import com.sun.hotspot.igv.layout.Link;
 import com.sun.hotspot.igv.layout.Vertex;
-import java.awt.Dimension;
 import java.awt.Point;
 import java.util.*;
 
 public class LinearLayoutManager implements LayoutManager {
 
-    public static final int LAYER_OFFSET = 0;
-
-    public enum Combine {
-
-        NONE,
-        SAME_INPUTS,
-        SAME_OUTPUTS
-    }
-    // Options
-    private int layerOffset;
-    private List<LayoutNode> nodes;
+    // Ranking determining the vertical node ordering.
     private Map<? extends Vertex, Integer> vertexRank;
 
-    private class LayoutNode {
-
-        public int x;
-        public int y;
-        public int height;
-        public Vertex vertex;
-
-        @Override
-        public String toString() {
-            return "Node " + vertex;
-        }
-    }
-
     public LinearLayoutManager(Map<? extends Vertex, Integer> vertexRank) {
-        this(Combine.NONE);
         this.vertexRank = vertexRank;
-    }
-
-    private LinearLayoutManager(Combine b) {
-        this.layerOffset = LAYER_OFFSET;
     }
 
     @Override
@@ -75,53 +46,24 @@ public class LinearLayoutManager implements LayoutManager {
 
     @Override
     public void doLayout(LayoutGraph graph, Set<? extends Link> importantLinks) {
-        System.out.println("LinearLayoutManager::doLayout(");
-        System.out.println("\tgraph: " + graph);
-        System.out.println("\timportantLinks: " + importantLinks);
-        System.out.println("\t)");
 
-        // Build data structures.
         assert (graph.getLinks().isEmpty());
+
+        // Sort vertices according to given rank.
         List<Vertex> vertices = new ArrayList<>(graph.getVertices());
         vertices.sort((Vertex a, Vertex b) ->
                       Integer.compare(vertexRank.getOrDefault(a, Integer.MAX_VALUE),
                                       vertexRank.getOrDefault(b, Integer.MAX_VALUE)));
 
-        nodes = new ArrayList<>();
-        for (Vertex v : vertices) {
-            LayoutNode node = new LayoutNode();
-            Dimension size = v.getSize();
-            node.height = (int) size.getHeight();
-            node.vertex = v;
-            node.x = 0;
-            nodes.add(node);
-        }
-
-        // Assign Y coordinates.
+        // Assign vertical coordinates in rank order.
         int curY = 0;
-        for (LayoutNode n : nodes) {
-            n.y = curY;
-            curY += n.height + layerOffset;
+        for (Vertex v : vertices) {
+            v.setPosition(new Point(0, curY));
+            curY += v.getSize().getHeight();
         }
 
-        // Write back result into vertices.
-        int minX = Integer.MAX_VALUE;
-        int minY = Integer.MAX_VALUE;
-        for (LayoutNode n : nodes) {
-            Point p = new Point(n.x, n.y);
-            n.vertex.setPosition(p);
-            minX = Math.min(minX, p.x);
-            minY = Math.min(minY, p.y);
-        }
-        for (Vertex v : graph.getVertices()) {
-            Point p = v.getPosition();
-            p.x -= minX;
-            p.y -= minY;
-            v.setPosition(p);
-        }
     }
 
     @Override
-    public void doRouting(LayoutGraph graph) {
-    }
+    public void doRouting(LayoutGraph graph) {}
 }
