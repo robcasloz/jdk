fb87be2ae807f3499a009b8be703daa23ed84ac8
Johan Sjölén <johan.sjolen@oracle.com>
2023-05-16 10:12:56 +0200

    Revert to regular pages

--- src/hotspot/share/memory/contiguousAllocator.hpp
+++ src/hotspot/share/memory/contiguousAllocator.hpp
@@ -17,33 +17,32 @@
 // Does not account for huge pages.
 class ContiguousAllocator {
 private:
+  // We're overriding os::reserve_memory and os::commit_memory
+  // This is to avoid having to call os::mmap on each commit.
   char* my_mmap() {
     // MAP_FIXED is intentionally left out, to leave existing mappings intact.
     const int flags = MAP_PRIVATE | MAP_NORESERVE | MAP_ANONYMOUS;
     char* addr = (char*)::mmap(nullptr, default_size, PROT_READ|PROT_WRITE, flags, -1, 0);
     if (addr != nullptr) {
-      MemTracker::record_virtual_memory_reserve(addr, default_size, CALLER_PC, mtChunk);
+      MemTracker::record_virtual_memory_reserve(addr, default_size, CALLER_PC, flag);
     }
     return addr == MAP_FAILED ? nullptr : (char*)addr;
   }
 public:
-  static const size_t chunk_size = 1 << 21; // 2 MiB
+  static size_t chunk_size() { return os::vm_page_size(); }
   static const size_t default_size = 1*G;
   // The number of unused-but-allocated chunks that we allow before madvising() that they're not needed.
   static const size_t slack = 4;
+  MEMFLAGS flag;
   const size_t size;
   char* start;
   char* offset;
   char* committed_boundary;
   ContiguousAllocator(size_t size, MEMFLAGS flag):
+    flag(flag),
     size(size), start(my_mmap()),
-    offset(nullptr),
-    committed_boundary(nullptr) {
-    offset = align_up(start, 1 << 21);
-    committed_boundary = offset;
-    assert((uintptr_t)offset % 4*K == 0, "not aligned");
-    assert((uintptr_t)offset % 64*K == 0, "not aligned");
-  }
+    offset(start),
+    committed_boundary(offset) {}
 
   ContiguousAllocator(MEMFLAGS flag)
     : ContiguousAllocator(default_size, flag){
@@ -53,7 +52,7 @@ public:
   }
   struct AllocationResult { void* loc; size_t sz; };
   AllocationResult alloc(size_t size) {
-    size_t chunk_aligned_size = align_up(size, chunk_size);
+    size_t chunk_aligned_size = align_up(size, chunk_size());
     char* p = this->offset;
     if (p + chunk_aligned_size >= start + this->size) {
       return {nullptr, 0};
@@ -68,13 +67,13 @@ public:
   }
 
   void reset_to(void* p) {
-    void* chunk_aligned_pointer = align_up(p, chunk_size);
+    void* chunk_aligned_pointer = align_up(p, chunk_size());
     offset = (char*)chunk_aligned_pointer;
     size_t unused_bytes = committed_boundary - offset;
 
     // We don't want to keep around too many pages that aren't in use,
     // so we ask the OS to throw away the physical backing, while keeping the memory reserved.
-    if (unused_bytes > slack*chunk_size) {
+    if (unused_bytes > slack*chunk_size()) {
       // Look into MADV_FREE/MADV_COLD
       ::madvise(offset, unused_bytes, MADV_DONTNEED);
       // The actual reserved region(s) might not cover this whole area, therefore
