67b107b2d2c72fb50272408e171bc02077775005
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2023-11-10 15:20:21 +0100

    Re-add const

--- src/hotspot/share/opto/node.hpp
+++ src/hotspot/share/opto/node.hpp
@@ -1133,7 +1133,7 @@ public:
   // reaching a boundary node, defined by is_boundary. Note: the function
   // definition appears after the complete type definition of Unique_Node_List.
   template <typename Callback, typename Check>
-  void visit_uses(Callback callback, Check is_boundary);
+  void visit_uses(Callback callback, Check is_boundary) const;
 
   // Visit all non-cast uses of the node, bypassing ConstraintCasts. Pattern:
   // this (-> ConstraintCast)* -> non_cast. In other words: find all non_cast
@@ -1729,7 +1729,7 @@ public:
 
 // Definition must appear after complete type definition of Unique_Node_List
 template <typename Callback, typename Check>
-void Node::visit_uses(Callback callback, Check is_boundary) {
+void Node::visit_uses(Callback callback, Check is_boundary) const {
   ResourceMark rm;
   // Unique_Node_List guarantees no duplicates in the worklist. Note that we
   // never pop anything from the worklist, since that could result in applying
