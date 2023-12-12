855ea7e2652935331563f9f5dd36547ce58f81a7
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2023-12-12 19:25:22 +0100

    TMP: Disable ArchiveHeapTestClass, ReplaceCriticalClassesForSubgraphs, and ResolvedReferencesNotNullTest when using -XX:-UseCompressedOops to reduce noise caused by unrelated failures

--- test/hotspot/jtreg/runtime/cds/appcds/sharedStrings/ResolvedReferencesNotNullTest.java
+++ test/hotspot/jtreg/runtime/cds/appcds/sharedStrings/ResolvedReferencesNotNullTest.java
@@ -25,7 +25,7 @@
  * @test ResolvedReferencesNotNullTest
  * @bug 8313638
  * @summary Testing resolved references array to ensure elements are non-null
- * @requires vm.cds.write.archived.java.heap
+ * @requires vm.cds.write.archived.java.heap & vm.opt.final.UseCompressedOops == true
  * @library /test/lib /test/hotspot/jtreg/runtime/cds/appcds
  * @build jdk.test.whitebox.WhiteBox ResolvedReferencesWb ResolvedReferencesTestApp
  * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
